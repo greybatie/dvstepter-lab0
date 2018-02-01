8da4c80ed1ce8c865118d9f8b0e9a232150f7798
DVStepter <DVStepter@users.noreply.github.com>
2018-01-31 18:24:32 -0800

    modified data analysis to proper folder

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Need to parse data
-f=open('/Users/darrellstepter/repos/school/NE204/Lab 0/dvstepter-lab0/lab0_spectral_data.txt')
+f=open('/Users/darrellstepter/repos/school/NE204/Lab0/dvstepter-lab0/lab0_spectral_data.txt')
 lines=f.readlines()[1:]
 
 
@@ -11,3 +11,16 @@ Ba133=[] # 80.997 keV (34%) 302.853 keV (18%) 356.017 keV (62%)
 Cs137=[] # 661.66 keV
 Co60=[] # 1173.2 keV and 1332.51 keV
 Eu152=[] # 344.28 keV (27%) 1112.1 keV (14%) 1408.1 keV (21%) 121.78 keV (26%)
+
+for x in lines:
+    x.split('\t')
+    y=[int(s) for s in x.split() if s.isdigit()]
+    Am241.append(y[0])
+    Ba133.append(y[1])
+    Cs137.append(y[2])
+    Co60.append(y[3])
+    Eu152.append(y[4])
+# testing that input vectors have the same length
+f.close()
+
+chan=list(range(1,len(Cs137)+1)) #number of channels:8192
